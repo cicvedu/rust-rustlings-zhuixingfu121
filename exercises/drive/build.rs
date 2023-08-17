cbd91eab8a1e92d870535af4625e4073cb35d063
zhuixingfu121 <te19980@163.com>
2023-08-17 10:24:48 +0800

    8.17

---
+++ exercises/drive/build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
+}
--- exercises/drive/drive2.rs
+++ exercises/drive/drive2.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 
 struct Foo {
@@ -15,6 +15,10 @@ fn raw_pointer_to_box(address: usize) -> Box<Foo> {
     // address is a pointer that points to heap.
     // construct Box from this address, and modify Foo's b field to 
     // the string "hello"
+    let raw_ptr = address as *mut Foo;
+    let mut box_foo = unsafe { Box::from_raw(raw_ptr) };
+    box_foo.b = Some("hello".to_owned());
+    box_foo
 }
 
 
